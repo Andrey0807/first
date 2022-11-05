c023b6c7f449341d0021d09777c529fba72da529
Andrey <basmsk@mail.ru>
2022-11-05 15:58:48 +0500

    first commit

---
+++ 04.11.2022/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// const arr = [1, 2, 3];
+
+// console.log(arr[2]);
+
+// const obj = {a: 1, 1: 2};
+
+// // obj.d = '1234';
+// obj['d'] = '1234';
+
+// console.log(obj['d']);
+// console.log(obj.d);
+
+// // console.log(alert('Дарова мужик'));
+// // const result = confirm('Пользователь ты тут?');
+// // console.log(result);
+
+// // const answer = prompt('Вам есть 18?', '18');
+// // console.log(typeof(answer));
+
+// const answers =[];
+
+// // answers[0] = prompt('Как ваше имя?', '');
+// // answers[1] = prompt('Как ваша фамилия?', '');
+// // answers[2] = prompt('Сколько вам лет?', '');
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = prompt('Как тебя зовут?');
+
+// alert(`Привет, ${user}`)
+
+// console.log(4 + +'5');
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked || isClose);
+
